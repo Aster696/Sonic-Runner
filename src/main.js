7ed45c910c5acd85ec1619b22ab308c0ed838fc6
Aster07102000 <aster.r@codezen.in>
2025-12-26 22:04:43 +0530

    |fist

--- src/main.js
+++ src/main.js
@@ -1,24 +1,36 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import k from './kaplayCtx';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+    sliceX: 8,
+    sliceY: 2,
+    anims: {
+        run: { from: 0, to: 7, loop: true, speed: 30 },
+        jump: { from: 8, to: 15, loop: true, speed: 100},
+    }
+});
+k.loadSprite("ring", "graphics/ring.png", {
+    sliceX: 16,
+    sliceY: 1,
+    anims: {
+        spin: { from: 0, to: 15, loop: true, speed: 30 }
+    }
+});
+k.loadSprite("motobug", "graphics/motobug.png", {
+    sliceX: 5,
+    sliceY: 1,
+    anims: {
+        run: { from: 0, to: 4, loop: true, speed: 8 }
+    }
+});
 
-setupCounter(document.querySelector('#counter'))
+k.loadFont("mania", "fonts/mania.ttf");
+
+k.loadSound("city", "sounds/city.mp3");
+k.loadSound("destroy", "sounds/Destroy.wav");
+k.loadSound("hyper-ring", "sounds/HiperRing.wav");
+k.loadSound("hurt", "sounds/Hurt.wav");
+k.loadSound("jump", "sounds/jump.wav");
+k.loadSound("ring", "sounds/Ring.wav");
+
